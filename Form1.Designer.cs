fa7a302be17004c782120fc27626802028baf108
忠磊 钱 <3511129836@qq.com>
2021-08-12 20:06:51 +0800

    全部信息获取硬件更新已经获取

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,11 +32,21 @@ namespace PCA
             this.components = new System.ComponentModel.Container();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -46,6 +56,14 @@ namespace PCA
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(159, 53);
             this.panel1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(0, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 26);
+            this.comboBox1.TabIndex = 0;
             // 
             // panel2
             // 
@@ -65,19 +83,69 @@ namespace PCA
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // comboBox1
+            // panel3
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(0, 13);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(156, 26);
-            this.comboBox1.TabIndex = 0;
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Location = new System.Drawing.Point(6, 132);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(229, 46);
+            this.panel3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Location = new System.Drawing.Point(12, 82);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(223, 40);
+            this.panel4.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label3);
+            this.panel5.Location = new System.Drawing.Point(241, 82);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(168, 40);
+            this.panel5.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 6);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(421, 205);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -85,6 +153,12 @@ namespace PCA
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -96,6 +170,13 @@ namespace PCA
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using OpenHardwareMonitor.Hardware;
 namespace PCA
 {
     public partial class Form1 : Form
@@ -50,6 +50,205 @@ namespace PCA
         {
             button1.Text ="扫描设备";
             comboBox1.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
+            label1.Text =Get_CPU_Name();
+            label2.Text =Get_GPU_Name();
+            label3.Text =Convert.ToString(Get_RAM_Total());
         }
+        /// <summary>
+        /// 获取CPU信息
+        /// </summary>
+        /// <returns></returns>
+        private string Get_CPU_Name()
+        {
+            string CPU_name =null;
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.Accept(updateVisitor);
+            computer.CPUEnabled =true;
+            CPU_name = computer.Hardware[0].Name;
+            computer.Close();
+            GC.Collect();
+            return CPU_name;
+        }
+        private int Get_CPU_Temp()
+        {
+            int Temp=0;
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+            computer.Accept(updateVisitor);
+            //computer.Open();
+            for (int j = 0; j < computer.Hardware[0].Sensors.Length; j++)
+            {
+                        //找到温度传感器
+                if (computer.Hardware[0].Sensors[j].SensorType == SensorType.Temperature && computer.Hardware[0].Sensors[j].Name == "CPU Package")
+                {
+                  Temp = Convert.ToInt32(computer.Hardware[0].Sensors[j].Value);
+                }
+            }
+            computer.Close();
+            GC.Collect();
+            return Temp;
+        }
+        private int Get_CPU_Load()
+        {
+            int Load = 0;
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+            computer.Accept(updateVisitor);
+
+           for (int j = 0; j < computer.Hardware[0].Sensors.Length; j++)
+             {
+                        //找到温度传感器
+               if (computer.Hardware[0].Sensors[j].SensorType == SensorType.Load && computer.Hardware[0].Sensors[j].Name =="CPU Total")
+                  {
+                    Load = Convert.ToInt32(computer.Hardware[0].Sensors[j].Value);
+                  }
+             }
+            computer.Close();
+            GC.Collect();
+            return Load;
+        }
+        /// <summary>
+        /// 获取GPU信息
+        /// </summary>
+        /// <returns></returns>
+        private string Get_GPU_Name()
+        {
+            string GPU_Name = null;
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.GPUEnabled = true;
+            computer.Accept(updateVisitor);
+            try
+            {
+                GPU_Name = computer.Hardware[1].Name;
+            }
+            catch
+            {
+
+                GPU_Name = computer.Hardware[0].Name;
+            }
+            return GPU_Name;
+        }
+        private int Get_GPU_Temp()
+        {
+            int Temp = 0;
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.GPUEnabled = true;
+            computer.Accept(updateVisitor);
+            try
+            {
+                for (int i = 0; i < computer.Hardware[1].Sensors.Length; i++)
+                {
+                    if (computer.Hardware[0].Sensors[i].SensorType == SensorType.Temperature)
+                    {
+                        Temp =Convert.ToInt32(computer.Hardware[1].Sensors[i].Value);
+                    }
+                }
+            }
+            catch
+            {
+                for (int i = 0; i < computer.Hardware[0].Sensors.Length; i++)
+                {
+                    if (computer.Hardware[0].Sensors[i].SensorType == SensorType.Temperature)
+                    {
+                        Temp = Convert.ToInt32(computer.Hardware[1].Sensors[i].Value);
+                    }
+                }
+            }
+            computer.Close();
+            GC.Collect();
+            return Temp;
+        }
+        private int Get_GPU_Load()
+        {
+            int Load = 0;
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.GPUEnabled = true;
+            computer.Accept(updateVisitor);
+            try
+            {
+                for (int i = 0; i < computer.Hardware[1].Sensors.Length; i++)
+                {
+                    if (computer.Hardware[1].Sensors[i].SensorType == SensorType.Load)
+                    {
+                        Load = Convert.ToInt32(computer.Hardware[1].Sensors[i].Value);
+                    }
+                }
+            }
+            catch 
+            {
+
+                for (int i = 0; i < computer.Hardware[0].Sensors.Length; i++)
+                {
+                    if (computer.Hardware[0].Sensors[i].SensorType == SensorType.Load)
+                    {
+                        Load = Convert.ToInt32(computer.Hardware[0].Sensors[i].Value);
+                    }
+                }
+            }
+            computer.Close();
+            GC.Collect();
+            return Load;
+        }
+        /// <summary>
+        /// 获取RAM信息
+        /// </summary>
+        /// <returns></returns>
+        private int Get_RAM_Load()
+        {
+            int Load = 0;
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.RAMEnabled = true;
+            computer.Accept(updateVisitor);
+            Load =Convert.ToInt32(computer.Hardware[0].Sensors[0].Value);
+            computer.Close();
+            GC.Collect();
+            return Load;
+        }
+        private int Get_RAM_Total()
+        {
+            int Total;
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.RAMEnabled = true;
+            computer.Accept(updateVisitor);
+            Total =Convert.ToInt32(computer.Hardware[0].Sensors[1].Value+ computer.Hardware[0].Sensors[2].Value);
+            computer.Close();
+            GC.Collect();
+            return Total;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text =Convert.ToString(Get_CPU_Temp());
+        }
+    }
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (IHardware subHardware in hardware.SubHardware)
+                subHardware.Accept(this);
+        }
+        public void VisitSensor(ISensor sensor) { }
+        public void VisitParameter(IParameter parameter) { }
     }
 }
